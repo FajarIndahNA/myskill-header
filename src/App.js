74a73e6e41275120a465b398462d379f626b0219
FajarIndahNA <fajarindah285@gmail.com>
2024-11-06 23:45:24 +0700

    About.js, penambahan resumeData.json, penghapusan App.css dan index.css, penambahan image profilepic

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ReactGA from 'react-ga';
+import { Component } from 'react';
+import $ from 'jquery';
+import Header from './components/Header';
+import About from "./components/About";
+// import Contact from "./components/Contact";
+// import Resume from "./components/Resume";
+// import Portfolio from "./components/Portofolio";
+// import Footer from "./components/Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header/>
+//     </div>
+//   );
+// }
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      foo: "bar",
+      resumeData: {}
+    };
+    // ReactGA.initialize("UA-110570651-1");
+    // ReactGA.pageview(window.location.pathname);
+
+    ReactGA.initialize("UA-110570651-1");
+    ReactGA.pageview(window.location.pathname);
+  }
+
+  getResumeData(){
+    $.ajax({
+      url:"./resumeData.json",
+      dataType: "json",
+      cache:false,
+      success: function(data) {
+        this.setState({
+          resumeData: data
+        });
+      }.bind(this),
+      error: function (xhr, status, err) {
+        console.log(err);
+        alert(err);
+      }
+    })
+  }
+
+  // memnggil komponent
+  componentDidMount(){
+    this.getResumeData();
+  }
+
+  render(){
+    return(
+      <div className='App'>
+        <Header data={this.state.resumeData.main}/>
+        <About data={this.state.resumeData.main}/>
+        {/* <Resume data={this.state.resumeData.resume}/> */}
+        {/* <Portofolio data={this.state.resumeData.portofolio}/> */}
+        {/* <Contact data={this.state.resumeData.main}/> */}
+        {/* <Footer data={this.state.resumeData.main}/> */}
+      </div>
+    )
+  }
+
 }
 
 export default App;
